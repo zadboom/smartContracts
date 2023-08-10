928cd312eb046fe5827e64dd7242c5ce24284e3e
Creator <113107338+zadboom@users.noreply.github.com>
2023-08-10 16:53:24 +0330

    Add files via upload

---
+++ hell0.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma  solidity >=0.8.11;
+
+//This is may fiert contact in remix
+
+contract FirstApp{
+
+//State variable save in the Blockchain
+
+string public helloStr1;
+ uint numbers=12;
+ bool statement=false;
+
+ constructor(){
+ helloStr1="heloooo";
+
+     
+ }
+ 
+ 
+ 
+
+
+        } 
+
+
+ contract MyContract{
+
+
+
+
+ }
+       
+/*
+
+[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "helloStr1",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+*/
